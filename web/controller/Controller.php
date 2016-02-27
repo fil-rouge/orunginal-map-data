8c2d5c9375f4c6103654b11d64d6b5112fa1e1a9
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-02-27 17:42:12 +0100

    Add main to check action request

---
+++ web/controller/Controller.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'Controller go go go ! <br/>';
---
+++ web/main.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once('controller/Controller.php');
+
+header("Access-Control-Allow-Origin: *");
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+if ((isset($_GET["action"])))
+{
+	echo 'ready to go ! <br/>';
+	//$c->analyseRequest($_GET["action"], (!empty($_GET["params"]))?$_GET["params"]:"");
+}
+else
+{
+	echo 'fail the Get..<br/>';
+}
