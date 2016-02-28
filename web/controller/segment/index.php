3fcca07efed1eb6820cf2b5e83f9165225b15407
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-02-28 22:29:04 +0100

    Add controller service to get gps points from segment in order

--- web/controller/segment/index.php
+++ web/controller/segment/index.php
@@ -9,6 +9,8 @@ echo 'includes ok';
 // Test insert
 //$insert = insert_pointgps(48.4546 , 7.554959);
 
+$points = get_segment_points_ordered(3,4);
+
 // Query 10 points from database
 $segmentsDB = get_segment_from_position(46,49,4.5,8);
 //$segmentsDB = get_segment_points_by_id(1);
@@ -30,7 +32,44 @@ foreach($segmentsDB as $segment)
 	// TODO !!!!!
 }
 
+/**
+*	Returns the gps points for a segment in the right order
+* 	starting with the $startpoint
+*/
+function get_segment_points_ordered($idSegment, $idStartPoint)
+{
+	// Call sql request
+	$pointsDB = get_segment_points_by_id($idSegment);
 
+	// Data processing
+	$points = array();
+	$pointsLength = count($pointsDB);
+	
+	if ($pointsDB[0]['id']==$idStartPoint)
+	{
+		// GPS points are in the RIGHT ORDER
+		foreach($pointsDB as $point) 
+		{
+			$points[] = new PointGPS($point['id'],$point['lat'],$point['lon']);
+		}
+	}
+	else if ($pointsDB[$pointsLength-1]['id']==$idStartPoint)
+	{
+		// GPS points are in the REVERSE ORDER
+		$i = pointsLength - 1;
+		foreach($pointsDB as $point) 
+		{
+			$points[$i] = new PointGPS($point['id'],$point['lat'],$point['lon']);
+			$i--;
+		}
+	}
+	else
+	{
+		echo '<br/><strong>ERROR:[get_segment_points_by_id] idStartPoint incorrect !!</strong><br/>';
+	}
+
+	return $points;
+}
 
 
 // Display view
