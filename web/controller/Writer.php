bb2ef2e629fa4bba4ff4f302b4148f5911ac618c
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-03-03 15:46:13 +0100

    Add method to write content in JSON in a file

--- web/controller/Writer.php
+++ web/controller/Writer.php
@@ -15,5 +15,19 @@ function append_to_file($fileName, $line)
 	fclose($myfile);
 }
 
+/**
+* Open $fileName & append $content at the end of the file
+*
+*/
+function append_to_file_json($fileName, $content)
+{
+	//	Open file 
+	$myfile = fopen($fileName, "a") or die("Unable to open file!");
 
+	//	Write content in JSON to the file
+	fputs($myfile, json_encode($content));
+	//var_dump($res);
+				
+	fclose($myfile);
+}
 
