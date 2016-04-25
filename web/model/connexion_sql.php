abff18a1ddd4c45944cf237ed30cfff6cef15840
arthurpichotutrera <arthur.pichot-utrera@orange.fr>
2016-04-25 20:58:24 +0200

    Fixe config for production branch

--- web/model/connexion_sql.php
+++ web/model/connexion_sql.php
@@ -4,13 +4,13 @@
 
 	// Connexion à la base de données
 	try
-	{/*
+	{
 		$dbopts = parse_url(getenv('DATABASE_URL'));
 
         $bdd = new PDO('pgsql:host=' . $dbopts["host"] . ';port='. $dbopts["port"] .';dbname=' . ltrim($dbopts["path"],'/') .
         			   ';user=' . $dbopts["user"] . ';password=' . $dbopts["pass"] );
-*/
-	    $bdd = new PDO('pgsql:host=' . $host . ';port=5432;dbname=' . $db . ';user=' . $username . ';password=' . $password );
+
+	    //$bdd = new PDO('pgsql:host=' . $host . ';port=5432;dbname=' . $db . ';user=' . $username . ';password=' . $password );
 	}
 	catch(Exception $e)
 	{
